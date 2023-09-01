ec9e93b2686fa347975b105378ea27e7ef928986
season <sionwer4@naver.com>
2023-09-01 21:17:47 +0900

    refactor: Change the mane of common properties of rooms and amenities

--- common/models.py
+++ common/models.py
@@ -6,8 +6,8 @@ class CommonModel(models.Model):
     Common Model Definition
     """
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
--- rooms/admin.py
+++ rooms/admin.py
@@ -4,9 +4,21 @@ from .models import Room, Amenity
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "kind", "owner")
+    list_display = ("name", "price", "kind", "owner", "created_at", "updated_at")
+    list_filter = (
+        "country",
+        "city",
+        "price",
+        "rooms",
+        "toilets",
+        "kind",
+        "amenities",
+        "created_at",
+        "updated_at",
+    )
 
 
 @admin.register(Amenity)
 class AmenityAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "description", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
