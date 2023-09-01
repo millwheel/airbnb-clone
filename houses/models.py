76dd5d0d0d953694bb09f2008604d2a005d34288
season <sionwer4@naver.com>
2023-09-01 18:45:33 +0900

    feat: Create rooms model

--- houses/models.py
+++ houses/models.py
@@ -3,11 +3,17 @@ from django.db import models
 
 class House(models.Model):
     """Model house"""
+
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField(verbose_name="price", help_text="Positive numbers only")
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="price", help_text="Positive numbers only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True, help_text="Does this house allow pets?")
+    pets_allowed = models.BooleanField(
+        default=True, help_text="Does this house allow pets?"
+    )
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
