3e9c24330d0ba8612368eadaf7046e61d0ea8c86
season <sionwer4@naver.com>
2023-09-01 20:33:55 +0900

    feat: Create rood admin

--- rooms/models.py
+++ rooms/models.py
@@ -8,6 +8,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = "shared_room", "Shared Room"
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -26,7 +27,16 @@ class Room(CommonModel):
     )
     amenities = models.ManyToManyField("rooms.Amenity")
 
+    def __str__(self):
+        return self.name
+
 
 class Amenity(CommonModel):
     name = models.CharField(max_length=150)
-    description = models.CharField(max_length=150, default="")
+    description = models.CharField(max_length=150, default="", blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
